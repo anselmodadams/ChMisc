77b6d5292e9706e16267ecdd72bd71275432f69b
Anselmo Daniel Adams <anselmo.d.adams@altinity.com>
2024-04-19 11:43:57 -0300

    Disabled experimental analyzer (CH 24.3+) and output handling changed

--- report/generate.sh
+++ report/generate.sh
@@ -2,4 +2,4 @@
 MY_PATH=$(readlink -f $0)
 MY_DIR=$(dirname $MY_PATH)
 cd $MY_DIR
-cat script.sql | clickhouse-local -mn > report.md
+cat script.sql | clickhouse-local --allow_experimental_analyzer 0 -mn | tee report.md > /dev/null
